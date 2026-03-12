a9715cef08da2c2bbcb13d138f8e1f08899ba9d8
jack-chambers-coder <jack-chambers@scas.nsw.edu.au>
2026-03-12 22:48:19 +0000

    Random number generator

--- Snorbo.py
+++ Snorbo.py
@@ -1,3 +1,9 @@
 print("I am Snorbo Snibblewink")
 print("This was done on ipad haha")
 print("Another little test...")
+list_1 = [0, 0, 0, 0, 0]
+for i in range[0, 3]:
+    num = random.randint(1,5)
+    list_1[num]"X"
+
+print(list_1)
