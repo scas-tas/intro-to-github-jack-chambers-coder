b4280510c460e6bfd1e1de93de7de51ca4295a09
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-03-20 15:04:52 +1100

    Finished animal dictionary

--- animal stuff.py
+++ animal stuff.py
@@ -1,32 +1,76 @@
 import random
 
-dictionary = {'attercop': {
-  'Type': "A spider",
-  "Base_Strength":
-  "Buffed_Strength" : get_random_buff() + dictionary["attercop"["A spider"]]
+def get_random_buff():
+    return random.randint(1, 12)
+
+def get_random_number():
+    return random.randint(1,4)
+
+def get_winner(animal_1, animal_2):
+   if dictionary1[animal_1()]
+
+
+
+
+dictionary1 = {'Attercop': {
+  'Type': "Spider",
+  "Base_Strength": 2,
   "amount" : get_random_number()
-
-
-  'batrachophagous': 'To feed on frogs.',
-    'chirotonsor': 'A barber.',
-      'deltiologist': 'A postcard collector.',
-        'erinaceous': 'Of hedgehogs.',
-          'favillous': 'Looking like ashes.',
-            'grimalkin': 'A cat.',
-              'nelipot': 'Someone who goes barefoot often.',
-                'pogonotomy': 'Cutting a beard.',
-                  'psithurism': 'The sound of wind in the trees.',
-                    'scolecophagous': 'To eat worms.',
-                      'xerophagy': 'Eating only dry food.'}
+  },
+    "Newt:" : {
+    "Type:": "Lizard",
+    "Base_Strength": 4,
+    "amount": get_random_number()
+    },
+      "Pismire" : {
+      "Type": "Ant",
+      "Base_Strength": 1,
+      "amount": get_random_number()
+      },
+        "Steed":{
+        "Type": "Horse",
+        "Base_Strength": 7,
+        "amount": get_random_number()
+        },
+          "Erne":{
+          "Type": "Eagle",
+          "Base_Strength": 7,
+          "amount": get_random_number()
+          },
+            "Nightbird":{
+            "Type": "Owl",
+            "Base_Strength": 3,
+            "amount": get_random_number()
+            },
+              "Warg":{
+              "Type": "Wolf",
+              "Base_Strength": 11,
+              "amount": get_random_number()
+              },
+                "Reynard":{
+                "Type": "Fox",
+                "Base_Strength": 10,
+                "amount": get_random_number()
+                },
+                  "Cony":{
+                  "Type": "Rabbit",
+                  "Base_Strength": 4,
+                  "amount": get_random_number()
+                  },
+                    "Shark":{
+                    "Type": "Seadog",
+                    "Base_Strength": 9,
+                    "amount": get_random_number()
+                    },
+                      "Paddok":{
+                      "Type": "Frog",
+                      "Base_Strength": 4,
+                      "amount": get_random_number()
+                      },
+}
 
 word = input("Enter a word: ")
-if word.lower() in dictionary:
-  print(dictionary[word.lower()])
+if word.lower() in dictionary1:
+  print(dictionary1[word.lower()])
 else:
   print("Not in the dictionary!")
-
-def get_random_buff():
-    return random.randit(1, 7)
-
-def get_random_number()
-    return random.randit(1,4)
