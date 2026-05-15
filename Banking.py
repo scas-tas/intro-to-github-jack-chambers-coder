d30078ba117885cfca51d78ec2eb627d89c35211
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-05-15 15:18:03 +1000

    Mainline

--- Banking.py
+++ Banking.py
@@ -3,6 +3,13 @@ class Account:
         self.number = number
         self.balance = balance
         self.owner = owner
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+
 
 # mainline
 
@@ -10,13 +17,37 @@ Jack = Account(235761, 7200000, "Jack")
 
 Snorbo = Account(79191, 52, "Snorbo")
 
-print(Jack.number)
-print(Snorbo.balance)
-print(Jack.owner)
+Blimbo = Account(10101, 10, "Blimbo")
 
-Fail = Account(10101, 10, "Fail")
 counter = 0
 
-for people in [Jack, Snorbo, Fail]:
+accounts = {"Jack": Jack, "Snorbo": Snorbo, "Blimbo": Blimbo}
+for people in [Jack, Snorbo, Blimbo]:
     counter += people.balance
-print(counter)
+print(f"Total bank amount: {counter} ")
+
+
+person = input("Enter your name: ")
+if person in accounts:
+    d_or_w = input("Would you like to withraw or deposit? (w/d)")
+    if d_or_w == "d":
+        deposit_num = int(input("How much would you like to deposit? "))
+        if deposit_num > 0:
+            print(f"Old balance: {accounts[person].balance}")
+            accounts[person].deposit(deposit_num)
+            print("New balance: {person.balance}")
+        else:
+            raise Exception("Invalid number")
+    elif d_or_w == "w":
+        withdraw_num = int(input("How much would you like to withdraw? "))
+        if withdraw_num > 0:
+            print(f"Old balance: {accounts[person].balance}")
+            accounts[person].withdraw(withdraw_num)
+            print(f"New balance: {accounts[person].balance}")
+        else:
+            raise Exception("Invalid number")
+    else:
+        raise Exception("invalid input")
+
+else:
+    raise Exception("Invalid name")
