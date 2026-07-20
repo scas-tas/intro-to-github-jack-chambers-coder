a744feec983d3eca930a8482a758991a31320f07
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-07-21 09:34:48 +1000

    Update 1

--- Assessment Task 2/Problem_1.py
+++ Assessment Task 2/Problem_1.py
@@ -1,12 +1,19 @@
 def combine_trucks(trucks, first_truck, second_truck):
     # TODO: return the total packages in first_truck and second_truck
-    return first_truck + second_truck
+        return first_truck + second_truck
+    
 
+# main()
 
-def main():
+def test_cases():
     trucks = [4, 7, 2, 6, 9]
     print(combine_trucks(trucks, trucks[1], trucks[3]))  # Expected: 13
     print(combine_trucks(trucks, trucks[0], trucks[2]))  # Expected: 6
 
+    trucks = [5,10,15,0,0]
+    print(combine_trucks(trucks, trucks[0], trucks[2]))  # Expected: 20
 
-main()
+    trucks = [0,0,0,0,1000]
+    print(combine_trucks(trucks, trucks[4], trucks[4]))  # Expected: 2000
+
+test_cases()
