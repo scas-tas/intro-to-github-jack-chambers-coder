3d9218e0fbe587afa07c0ade0d7aaeb0a54ed48f
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-08-05 15:02:29 +1000

    Problem 3 Draft 1

--- Assessment Task 2/Problem_3.py
+++ Assessment Task 2/Problem_3.py
@@ -1,2 +1,25 @@
-for i in range(3):
-    print("hello")
+
+def add_item(inventory, name, quantity) -> None:
+    if name in inventory:
+        inventory[name] += quantity
+    else:
+        inventory[name] = quantity
+    inv = inventory
+def remove_item(inventory: dict, name: str, quantity: int) -> None:
+    if name in inventory:
+        inventory[name] = max(0, inventory[name] - quantity)
+ 
+def get_stock_report(inventory: dict) -> str:
+    for fruit in inventory:
+        if inventory[fruit] > 0:
+            print(f"{fruit}: {inventory[fruit]}")
+
+inv = {}
+add_item(inv, 'apples', 10)
+add_item(inv, 'bananas', 5)
+get_stock_report(inv)
+add_item(inv, 'apples', 5)
+remove_item(inv, 'bananas', 10)
+get_stock_report(inv)
+remove_item(inv, 'oranges', 3)
+get_stock_report(inv)
