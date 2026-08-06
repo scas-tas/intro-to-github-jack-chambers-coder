b606dab16a1f1c4c6cd21df019e24466f00d93e7
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-08-06 20:31:54 +1000

    Update Problem_4.py

--- Assessment Task 2/Problem_4.py
+++ Assessment Task 2/Problem_4.py
@@ -1,27 +1,41 @@
-def encode(message, shift) -> str:
-    lower_min = 97
-    lower_max = 122
-    upper_min = 65
-    upper_max = 90
+lower_min = 97
+lower_max = 122
+upper_min = 65
+upper_max = 90
 
+
+def encode(message, shift) -> str:
     for letter in message:
         if letter.isalpha():
             if letter.isupper():
                 num = ord(letter) - upper_min
-                num = (num + shift) % upper_max
+                num = (num + shift) % (upper_max - upper_min + 1) # new number % range
                 print(chr(num+upper_min), end="")
             else:
                 num = ord(letter) - lower_min
-                #print(f"test no 1: {num}")
-                num = (num + shift) % (lower_max - lower_min)
-                #print(f"test no 2: {num}")
-                print(chr(num+lower_min), end="")
-            
+                num = (num + shift) % (lower_max - lower_min + 1) # new number % range
+                print(chr(num+lower_min), end="")       
         else:
             print(letter, end="")
     print()
  
 def decode(message, shift) -> str:
-    return
+    for letter in message:
+        if letter.isalpha():
+            if letter.isupper():
+                num = ord(letter) - upper_min
+                num = (num - shift) % (upper_max - upper_min + 1) # new number % range
+                print(chr(num+upper_min), end="")
+            else:
+                num = ord(letter) - lower_min
+                num = (num - shift) % (lower_max - lower_min + 1) # new number % range
+                print(chr(num+lower_min), end="")         
+        else:
+            print(letter, end="")
+    print()
 
 encode('Hello', 3)
+encode('Hello, World!', 3)
+decode('Khoor', 3)
+encode('xyz', 3)
+decode('abc', 3)
