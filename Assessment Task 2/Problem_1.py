b74c92995d9c33822e945e990aea98f1661e00ef
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-08-06 12:56:15 +1000

    Update to Problem 1

--- Assessment Task 2/Problem_1.py
+++ Assessment Task 2/Problem_1.py
@@ -1,19 +1,21 @@
 def combine_trucks(trucks, first_truck, second_truck):
     # TODO: return the total packages in first_truck and second_truck
-        return first_truck + second_truck
+    if first_truck != second_truck:
+        return trucks[first_truck-1] + trucks[second_truck - 1]
+    else:
+        return f"{trucks[first_truck-1] + trucks[second_truck - 1]} Note: Boundary case, real value: {trucks[first_truck-1]}"
     
 
 # main()
 
 def test_cases():
     trucks = [4, 7, 2, 6, 9]
-    print(combine_trucks(trucks, trucks[1], trucks[3]))  # Expected: 13
-    print(combine_trucks(trucks, trucks[0], trucks[2]))  # Expected: 6
+    print(combine_trucks(trucks, 2, 4))  # Expected: 13
 
     trucks = [5,10,15,0,0]
-    print(combine_trucks(trucks, trucks[0], trucks[2]))  # Expected: 20
+    print(combine_trucks(trucks, 1, 3))  # Expected: 20
 
     trucks = [0,0,0,0,1000]
-    print(combine_trucks(trucks, trucks[4], trucks[4]))  # Expected: 2000
+    print(combine_trucks(trucks, 5, 5))  # Expected: 2000
 
 test_cases()
