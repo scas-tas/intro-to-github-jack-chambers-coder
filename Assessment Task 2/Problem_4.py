a2706c1a7c8ff6ed331e9dfd7c0edac3e8b17afc
Jack Snibblewink The First <jack-chambers@scas.nsw.edu.au>
2026-08-06 20:54:01 +1000

    Update Problem_4.py

--- Assessment Task 2/Problem_4.py
+++ Assessment Task 2/Problem_4.py
@@ -2,40 +2,40 @@ lower_min = 97
 lower_max = 122
 upper_min = 65
 upper_max = 90
-
-
 def encode(message, shift) -> str:
+    result = ""
     for letter in message:
         if letter.isalpha():
             if letter.isupper():
                 num = ord(letter) - upper_min
                 num = (num + shift) % (upper_max - upper_min + 1) # new number % range
-                print(chr(num+upper_min), end="")
+                result += chr(num+upper_min)
             else:
                 num = ord(letter) - lower_min
                 num = (num + shift) % (lower_max - lower_min + 1) # new number % range
-                print(chr(num+lower_min), end="")       
+                result += chr(num+lower_min)      
         else:
-            print(letter, end="")
-    print()
+            result += letter
+    return result
  
 def decode(message, shift) -> str:
+    result = ""
     for letter in message:
         if letter.isalpha():
             if letter.isupper():
                 num = ord(letter) - upper_min
                 num = (num - shift) % (upper_max - upper_min + 1) # new number % range
-                print(chr(num+upper_min), end="")
+                result += chr(num+upper_min)
             else:
                 num = ord(letter) - lower_min
                 num = (num - shift) % (lower_max - lower_min + 1) # new number % range
-                print(chr(num+lower_min), end="")         
+                result += chr(num+lower_min)     
         else:
-            print(letter, end="")
-    print()
+            result += letter
+    return result
 
-encode('Hello', 3)
-encode('Hello, World!', 3)
-decode('Khoor', 3)
-encode('xyz', 3)
-decode('abc', 3)
+print(encode('Hello', 3))
+print(encode('Hello, World!', 3))
+print(decode('Khoor', 3))
+print(encode('xyz', 3))
+print(decode('abc', 3))
